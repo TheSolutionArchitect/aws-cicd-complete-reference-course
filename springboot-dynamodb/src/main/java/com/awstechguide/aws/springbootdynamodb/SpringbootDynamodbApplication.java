b363c5396fc8e0c4ff61e05692bb778508dfe33e
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2020-11-09 20:32:14 -0500

    sprongboot project

---
+++ springboot-dynamodb/src/main/java/com/awstechguide/aws/springbootdynamodb/SpringbootDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.awstechguide.aws.springbootdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDynamodbApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Spring boot app");
+		SpringApplication.run(SpringbootDynamodbApplication.class, args);
+		}
+	}
